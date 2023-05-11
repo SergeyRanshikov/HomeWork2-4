c9b7daa1f05cadd2b8f2fdf2fc0d86d8e5c10153
ranss@rambler.ru <ranss@rambler.ru>
2023-05-11 15:11:39 +0300

    Начальный коммит

---
+++ src/main/java/com/input/loginoptions/LoginOptionsApplication.java
@@ -0,0 +1,13 @@
+package com.input.loginoptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginOptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginOptionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/input/loginoptions/LoginOptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.input.loginoptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginOptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
